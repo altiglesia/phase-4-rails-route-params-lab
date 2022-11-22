8f3f7356266802388aece145d9a50b498ea0a81e
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-11-22 11:05:33 -0500

    commit tree 411d20e270907081dd21eef4ab2d4d04c9e18cfe

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,5 +3,7 @@ class Student < ApplicationRecord
   def to_s
     "#{first_name} #{last_name}"
   end
-
+  
+  scope :by_name, ->(name) { where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%") }
+  
 end
